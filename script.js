ab31c60b183a5b5a0bc982727394ea28be06ddb4
happycooldude <pieterhuisman2000@gmail.com>
2021-08-31 14:28:28 +0200

    Bestanden aangemaakt en begin aan start pagina gemaakt

---
+++ script.js
@@ -0,0 +1,7 @@
+// let currentSubject = 0;
+
+// const title = document.getElementById("title");
+// const statement = document.getElementById("statement");
+
+// title.innerHTML = subjects[currentSubject].title;
+// statement.innerHTML = subjects[currentSubject].statement;
