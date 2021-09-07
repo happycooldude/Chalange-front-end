a2c81a7fd97bcac3c66c938aa6d2e588ebf57d76
happycooldude <pieterhuisman2000@gmail.com>
2021-09-07 13:55:05 +0200

    vergeten eerder te committen

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-// let currentSubject = 0;
+function start(){
+    document.getElementById("startContainer").classList.remove("shown");
+    document.getElementById("startContainer").classList.add("hidden");
+    document.getElementById("vraagContainer").classList.remove("hidden");
+    document.getElementById("vraagContainer").classList.add("shown");
+}
 
-// const title = document.getElementById("title");
-// const statement = document.getElementById("statement");
+let currentSubject = 0;
+let answerArray = [];
+let vraagArray = [];
 
-// title.innerHTML = subjects[currentSubject].title;
-// statement.innerHTML = subjects[currentSubject].statement;
+const eensBTN = document.getElementById("eens");
+const geenVbeideBTN = document.getElementById("geenVbeide");
+const oneensBTN = document.getElementById("oneens");
+const overslaanBTN = document.getElementById("overslaan");
+
+eensBTN.onclick = checkAnswer;
+geenVbeideBTN.onclick = checkAnswer;
+oneensBTN.onclick = checkAnswer;
+overslaanBTN.onclick = skipQuestion;
+
+const numberOfQuestions = subjects.length;
+const title = document.getElementById("title");
+const statement = document.getElementById("statement");
+
+title.innerHTML = subjects[currentSubject].title;
+statement.innerHTML = subjects[currentSubject].statement;
+
+function checkAnswer(){
+    if (answerArray.length < numberOfQuestions) {
+        console.log(currentSubject);
+        answerArray.push(this.id);
+        vraagArray.push(currentSubject);
+        console.log(answerArray);
+        console.log(vraagArray);
+        currentSubject ++;
+        title.innerHTML = subjects[currentSubject].title;
+        statement.innerHTML = subjects[currentSubject].statement;
+    } else {
+        answerArray.pop();
+        vraagArray.pop();
+        title.innerHTML = "Einde"
+        statement.innerHTML = "U heeft alle vragen beantwoord, wilt u nog teruggaan?"
+    }
+
+}
+
+function skipQuestion(){
+    answerArray.push("skip");
+    vraagArray.push(currentSubject);
+    console.log(answerArray);
+    console.log(vraagArray);
+    currentSubject ++;
+    title.innerHTML = subjects[currentSubject].title;
+    statement.innerHTML = subjects[currentSubject].statement;
+}
