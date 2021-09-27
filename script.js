8b3fa2ba14b1fa9010f025d6c93a6ac88582ce08
happycooldude <pieterhuisman2000@gmail.com>
2021-09-27 12:05:37 +0200

    Veel comments gemaakt bij de functie die snoek gemaakt heeft om het beter te begrijpen

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ const geenVbeideBTN = document.getElementById("geenVbeide");
 const oneensBTN = document.getElementById("contra");
 const overslaanBTN = document.getElementById("overslaan");
 const terugBTN = document.getElementById("back");
+const gotoResult = document.getElementById("gotoResult");
 
 eensBTN.onclick = checkAnswer;
 geenVbeideBTN.onclick = checkAnswer;
 oneensBTN.onclick = checkAnswer;
 overslaanBTN.onclick = checkAnswer;
 terugBTN.onclick = back;
+gotoResult.onclick = gotoResults;
 
 const numberOfQuestions = subjects.length;
 const title = document.getElementById("title");
@@ -36,7 +38,7 @@ function checkAnswer() {
         title.innerHTML = subjects[currentSubject].title;
         statement.innerHTML = subjects[currentSubject].statement;
     } else {
-        gotoResults();
+        gotobelangrijk();
     }
 }
 
@@ -67,9 +69,25 @@ function back() {
     vraagArray.pop(currentSubject);
 }
 
-function gotoResults() {
+function gotobelangrijk(){
     document.getElementById("vraagContainer").classList.remove("shown");
     document.getElementById("vraagContainer").classList.add("hidden");
+    document.getElementById("belangrijk").classList.remove("hidden");
+    document.getElementById("belangrijk").classList.add("shown");
+}
+
+createImportant();
+
+function createImportant(){
+
+    for (let index = 0; index < subjects.length; index++) {
+        document.getElementById("belangrijk").innerHTML += subjects[index].title + ", ";
+    }
+}
+
+function gotoResults() {
+    document.getElementById("belangrijk").classList.remove("shown");
+    document.getElementById("belangrijk").classList.add("hidden");
     document.getElementById("uitslagContainer").classList.remove("hidden");
     document.getElementById("uitslagContainer").classList.add("shown");
 
@@ -77,26 +95,27 @@ function gotoResults() {
 }
 
 function partyAssignPoints() {
-    for (let j in parties) {        // parties[0].name = "PVV"
-        let points = 0
-        let party = parties[j];
-        let currentPartyName = party.name;  // "PVV"
+    for (let tellerParties in parties) {        // Loop door parties heen.
+        let points = 0              //Maakt de var points aan met de waarde 0. tellerttellerPartiese.
+        let party = parties[tellerParties];     //Maakt een var aan met de waarde van de current positie in de parties array.
+        let currentPartyName = party.name;  //Maakt een var met de naam van de partij waar we nu mee bezig zijn.
 
-        for (let x in subjects) {       // subjects[0].title = "Bindend referendum"     x = 0 ... 3
-            let currentSubject = subjects[x];
+        for (let tellerSubjects in subjects) {       // Loopt door de array subjects heen.
+            let currentSubject = subjects[tellerSubjects];   //Maakt een var aan met de waarde van het subject waar we nu mee bezig zijn.
             // kijk welk antwoord de huidige partij heeft gegevens
-            let currentPartyPosition = null;
-            for (let pos in currentSubject.parties) {
-                if (currentSubject.parties[pos].name === currentPartyName) {
-                    currentPartyPosition = currentSubject.parties[pos].position;
+            let currentPartyPosition = null;        //Maakt variable aan voor de position van de partij waar we nu mee bezig zijn.
+            for (let tellerSubjectParties in currentSubject.parties) {   //loop door de parties van de subject waar we nu mee bezig zijn.
+                if (currentSubject.parties[tellerSubjectParties].name === currentPartyName) {   //Kijk of de naam van de partij overeenkomt met de partijen uit deze vraag.
+                    currentPartyPosition = currentSubject.parties[tellerSubjectParties].position;   //Als de namen overeenkomen stop de positie van de partij in de var currentPartyPosition.
                 }
             }
-            // kijk of het antwoord van de PVV overeenkomt met mijn gegeven antwoord
-            if (answerArray[x] === currentPartyPosition) {
-                points++;
+            //Je bent uit de loop.
+            // kijk of de mening van de partij overeenkomt met het gegeven antwoord
+            if (answerArray[tellerSubjects] === currentPartyPosition) { //Als de mening van de partij overeenkomt met het gegeven antwoord
+                points++;                   //Dan +1 punt bij de variable optellen.
             }
         }
-        parties[j].points = points;
+        parties[tellerParties].pointsVariable = points;     //Geeft de puntervar aan de parties array, en geeft het de waarde van de punten mee.
         
     }
 
@@ -104,15 +123,15 @@ function partyAssignPoints() {
     sortPoints();
     function sortPoints(){
             parties.sort(function (x, y){
-            return y.points - x.points;
+            return y.pointsVariable - x.pointsVariable;
         });
         console.table(parties);
     }
 
     //laat de lijst met partijen en de punten zien op het scherm
     for(let g in parties){
-    document.getElementById("uitslag").innerHTML += parties[g].name + ", ";
-    document.getElementById("uitslag").innerHTML += parties[g].points + " punten ";
-    document.getElementById("uitslag").innerHTML += parties[g].size + " zetels " + "<br>";
+            document.getElementById("uitslag").innerHTML += parties[g].name + ", ";
+            document.getElementById("uitslag").innerHTML += parties[g].pointsVariable + " punten ";
+            document.getElementById("uitslag").innerHTML += parties[g].size + " zetels " + "<br>";
     }
 }
