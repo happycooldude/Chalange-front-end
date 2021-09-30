373f52d5274b7b08ed7cc2d7d8ae768c8c1c4c15
happycooldude <pieterhuisman2000@gmail.com>
2021-09-30 11:28:58 +0200

    Dubbele puntentelling werkend gekregen met hulp van Snoek

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ function start() {
     document.getElementById("vraagContainer").classList.add("shown");
 }
 
+const groot = 10;
 let currentSubject = 0;
 let answerArray = [];
 let vraagArray = [];
+
 
 const eensBTN = document.getElementById("pro");
 const geenVbeideBTN = document.getElementById("geenVbeide");
@@ -15,6 +17,8 @@ const oneensBTN = document.getElementById("contra");
 const overslaanBTN = document.getElementById("overslaan");
 const terugBTN = document.getElementById("back");
 const gotoResult = document.getElementById("gotoResult");
+const seculair = document.getElementById("seculair");
+const grotePartij = document.getElementById("groot");
 
 eensBTN.onclick = checkAnswer;
 geenVbeideBTN.onclick = checkAnswer;
@@ -69,7 +73,7 @@ function back() {
     vraagArray.pop(currentSubject);
 }
 
-function gotobelangrijk(){
+function gotobelangrijk() {
     document.getElementById("vraagContainer").classList.remove("shown");
     document.getElementById("vraagContainer").classList.add("hidden");
     document.getElementById("belangrijk").classList.remove("hidden");
@@ -77,8 +81,7 @@ function gotobelangrijk(){
     createImportant();
 }
 
-function createImportant(){
-    //maak een grid van 3 breed aan met alle vragen
+function createImportant() { //maak een grid van 3 breed aan met alle vragen
     for (let index = 0; index < subjects.length; index++) {
         var container = document.getElementById("grid");
         var cell = document.createElement("div");
@@ -95,63 +98,84 @@ function createImportant(){
 
 var checkboxArray = [];
 
-function testcheckboxes(){
+function testcheckboxes() { //Loop door de checkboxes heen en laat de waardes in een array zien.
     for (let checkboxID = 0; checkboxID < subjects.length; checkboxID++) {
         var checkbox = document.getElementById(checkboxID);
         if (checkbox.checked) {
-            checkboxArray.push(subjects[checkboxID].title + " ID " + checkboxID + " VALUE " + checkbox.value)
+            subjects[checkboxID].weight = 2;
+            // checkboxArray.question = subjects[checkboxID].title;
+            // checkboxArray.id = checkboxID;
+            // checkboxArray.value = checkbox.value;
+            // checkboxArray.push(subjects[checkboxID].title + " ID " + checkboxID + " VALUE " + checkbox.value);
+        }else{
+            subjects[checkboxID].weight = 1;
         }
     }
-    console.log(checkboxArray);
+    console.table(subjects);
 }
-
 
 function gotoResults() {
     document.getElementById("belangrijk").classList.remove("shown");
     document.getElementById("belangrijk").classList.add("hidden");
     document.getElementById("uitslagContainer").classList.remove("hidden");
     document.getElementById("uitslagContainer").classList.add("shown");
-    
+
     testcheckboxes();
     partyAssignPoints();
 }
 
 function partyAssignPoints() {
-    for (let tellerParties in parties) {        // Loop door parties heen.
-        let points = 0              //Maakt de var points aan met de waarde 0. tellerttellerPartiese.
-        let party = parties[tellerParties];     //Maakt een var aan met de waarde van de current positie in de parties array.
-        let currentPartyName = party.name;  //Maakt een var met de naam van de partij waar we nu mee bezig zijn.
+    for (let tellerParties in parties) { // Loop door parties heen.
+        let points = 0 //Maakt de var points aan met de waarde 0. tellerttellerPartiese.
+        let party = parties[tellerParties]; //Maakt een var aan met de waarde van de current positie in de parties array.
+        let currentPartyName = party.name; //Maakt een var met de naam van de partij waar we nu mee bezig zijn.
 
-        for (let tellerSubjects in subjects) {       // Loopt door de array subjects heen.
-            let currentSubject = subjects[tellerSubjects];   //Maakt een var aan met de waarde van het subject waar we nu mee bezig zijn.
+        for (let tellerSubjects in subjects) { // Loopt door de array subjects heen.
+            let currentSubject = subjects[tellerSubjects]; //Maakt een var aan met de waarde van het subject waar we nu mee bezig zijn.
             // kijk welk antwoord de huidige partij heeft gegevens
-            let currentPartyPosition = null;        //Maakt variable aan voor de position van de partij waar we nu mee bezig zijn.
-            for (let tellerSubjectParties in currentSubject.parties) {   //loop door de parties van de subject waar we nu mee bezig zijn.
-                if (currentSubject.parties[tellerSubjectParties].name === currentPartyName) {   //Kijk of de naam van de partij overeenkomt met de partijen uit deze vraag.
-                    currentPartyPosition = currentSubject.parties[tellerSubjectParties].position;   //Als de namen overeenkomen stop de positie van de partij in de var currentPartyPosition.
+            let currentPartyPosition = null; //Maakt variable aan voor de position van de partij waar we nu mee bezig zijn.
+            for (let tellerSubjectParties in currentSubject.parties) { //loop door de parties van de subject waar we nu mee bezig zijn.
+                if (currentSubject.parties[tellerSubjectParties].name === currentPartyName) { //Kijk of de naam van de partij overeenkomt met de partijen uit deze vraag.
+                    currentPartyPosition = currentSubject.parties[tellerSubjectParties].position; //Als de namen overeenkomen stop de positie van de partij in de var currentPartyPosition.
                 }
             }
             //Je bent uit de loop.
             // kijk of de mening van de partij overeenkomt met het gegeven antwoord
             if (answerArray[tellerSubjects] === currentPartyPosition) { //Als de mening van de partij overeenkomt met het gegeven antwoord
-                points++;                   //Dan +1 punt bij de variable optellen.
+                points++; //Dan +1 punt bij de variable optellen.
+                if(subjects[tellerSubjects].weight === 2){
+                    points++;
+                }
             }
         }
-        parties[tellerParties].pointsVariable = points;     //Geeft de puntervar aan de parties array, en geeft het de waarde van de punten mee.
+        parties[tellerParties].pointsVariable = points; //Geeft de puntervar aan de parties array, en geeft het de waarde van de punten mee.
     }
 
-    //dont question it it works
+    //NIET AANKOMEN it works !
     sortPoints();
-    function sortPoints(){
-            parties.sort(function (x, y){
+
+    function sortPoints() {
+        parties.sort(function (x, y) {
             return y.pointsVariable - x.pointsVariable;
         });
         console.table(parties);
     }
 
     //laat de lijst met partijen en de punten zien op het scherm
-    for(let g in parties){
-            document.getElementById("uitslag").innerHTML += parties[g].name + ", ";
-            document.getElementById("uitslag").innerHTML += parties[g].pointsVariable + " punten " + "<br>";
-    }
+    
+        if(seculair.checked){
+            alert("seculair is checked");
+            for (let g in parties) {
+                document.getElementById("uitslag").innerHTML += parties[g].name + ", ";
+                document.getElementById("uitslag").innerHTML += parties[g].pointsVariable + " punten " + "<br>";
+            }
+        }
+        if(grotePartij.checked){
+            alert("groot is checked")
+            for (let g in parties) {
+                document.getElementById("uitslag").innerHTML += parties[g].name + ", ";
+                document.getElementById("uitslag").innerHTML += parties[g].pointsVariable + " punten " + "<br>";
+            }
+        }
+    
 }
